be1b2d1c51f3e9e90ee0339082ae36095604cc70
Pj Malva <pjmalva@gmail.com>
2023-10-13 13:29:06 -0300

    2023

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,12 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {  "javascript", "bash", "css", "dart", "dockerfile", "gitignore", "go", "html", "json", "json5", "php", "python", "svelte", "tsx", "typescript", "vue", "xml", "rust", "c", "lua", "vim", "vimdoc", "query" },
+
+  sync_install = false,
+
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
---
+++ after/plugin/zenmode.lua
@@ -0,0 +1,29 @@
+vim.keymap.set("n", "<leader>zm", function()
+    require("zen-mode").setup {
+        window = {
+            width = 90,
+            options = { }
+        },
+    }
+    require("zen-mode").toggle()
+    vim.wo.wrap = false
+    vim.wo.number = true
+    vim.wo.rnu = true
+    ColorVim()
+end)
+
+
+vim.keymap.set("n", "<leader>zM", function()
+    require("zen-mode").setup {
+        window = {
+            width = 80,
+            options = { }
+        },
+    }
+    require("zen-mode").toggle()
+    vim.wo.wrap = false
+    vim.wo.number = false
+    vim.wo.rnu = false
+    vim.opt.colorcolumn = "0"
+    ColorVim()
+end)
